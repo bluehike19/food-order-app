942790a1916460ca3e7edc03e6cebd3d83b741f0
simo <wambuisimon712@gmail.com>
2023-10-17 23:07:04 +0300

    yes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// module.exports = {
+
+// }
